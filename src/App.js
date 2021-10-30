94c187d8fdf895d281c22ba530ddccb69fe370fd
AleexZ11 <aleexzorzin@gmail.com>
2021-10-30 14:43:16 -0300

    gh pages solved

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pagina de Alex Zorzin. Se viene un gran proyecto.
         </p>
         <a
           className="App-link"
