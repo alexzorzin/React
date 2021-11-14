4fb19388497f90dea5b1f9ce9b39c4248955240f
AleexZ11 <aleexzorzin@gmail.com>
2021-11-13 21:40:50 -0300

    desafio menu ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pagina de Alex Zorzin. Se viene un gran proyecto.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar/>
   );
 }
 
