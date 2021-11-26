bb6286b64b0f478d2aed59ef855a36e906b0d383
AleexZ11 <aleexzorzin@gmail.com>
2021-11-26 19:11:00 -0300

    desafio maps y promises

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useState, useEffect } from 'react';
 import { NavBar } from './components/NavBar/NavBar';
-import { Item } from './components/Item/Item';
+import ItemListContainer from './components/container/ItemListContainer/ItemListContainer';
 
 function App() {
-
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    Item
-      .then(data => {
-        console.log('llamo a la api de productos')
-        setProducts(data)
-      })
-      .finally(() => setLoading(false))
-  }, [])
 
   return (
     <div className="App">
       {/* NAVBAR */}
       <header>
         <NavBar />
-      </header>
-      {loading ? <h1>Espere por favor...</h1> : products.map(prod => <div key={prod.id} className="card w-50 mt-5 mb-5 container" >
-        <div className="card-header">
-          {prod.name}
-        </div>
-        <div className="card-body">
-          <img className="img-fluid w-25" src={prod.img} alt="foto" />
-          <div>
-            {'Talle: ' + prod.size}
-          </div>
-          <div>
-            {'$' + prod.price}
-          </div>
-        </div>
-        <div className="card-footer">
-
-          <button className="btn btn-outline-primary btn-block">
-            Comprar
-          </button>
-
+        </header>
+        {/* ITEMLISTCONTAINER */}
+        <div>
+        <ItemListContainer/>
         </div>
 
-      </div>
-      )}
     </div>
   );
 }
---
+++ src/components/Stock.js
@@ -0,0 +1,12 @@
+export const Stock = [
+    
+        { id: 1, stock: 4, name: 'Nike AirForce One', size: 8, price: 16999, img:'https://www.courir.es/dw/image/v2/BCCL_PRD/on/demandware.static/-/Sites-master-catalog-courir/default/dw26559699/images/hi-res/001488854_101.png?sw=600&sh=600&sm=fit' },
+        { id: 2, stock: 15, name: 'Nike Court Vision Low', size: 7.5, price: 11000, img: 'https://http2.mlstatic.com/D_NQ_NP_812575-MLA40853945365_022020-O.webp' },
+        { id: 3, stock: 42, name: 'Adidas Advantage', size: 9, price: 9199, img: 'https://http2.mlstatic.com/D_NQ_NP_667341-MLA48011270907_102021-O.webp' },
+        { id: 4, stock: 58, name: 'Puma Electron E', size: 9, price: 9599, img: 'https://http2.mlstatic.com/D_NQ_NP_792759-MLA47957265972_102021-O.webp' },
+        { id: 5, stock: 11, name: 'Adidas Superstar', size: 7, price: 16000, img: 'https://images.footlocker.com/is/image/FLEU/316772989204_01?wid=763&hei=538&fmt=png-alpha' },
+        { id: 6, stock: 3, name: 'Nike Air Max 97', size: 8, price: 25599, img: 'https://www.moovbydexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw26473373/products/NI_921733-603/NI_921733-603-1.JPG' },
+        { id: 7, stock: 34, name: 'Vans Old Skool', size: 7.5, price: 10999, img: 'https://www.moovbydexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw0dadcf9f/products/VA_VN000D3HY28/VA_VN000D3HY28-1.JPG' },
+        { id: 8, stock: 19, name: 'Puma City Rider', size: 7.5, price: 14000, img: 'https://www.moovbydexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwefbf0983/products/PU_382044-03/PU_382044-03-1.JPG' },
+        { id: 9, stock: 21, name: 'Adidas Stan Smith', size: 9, price: 17999, img: 'https://www.moovbydexter.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw727144c7/products/AD_FY5460/AD_FY5460-1.JPG' }
+    ];
